951b6cd6f14bf4d08ab69ac03335295cd11caac4
Chase Mulder <77690673+ChaseMulder@users.noreply.github.com>
2022-04-27 13:54:25 -0400

    Delete CompSci Code/battleship directory

--- CompSci Code/battleship/sleepTimer.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SLEEPTIMER_HPP_INCLUDED
-#define SLEEPTIMER_HPP_INCLUDED
-
-/* Sleep Timer */
-using namespace std;
-using namespace std::this_thread; // sleep_for, sleep_until
-using namespace std::chrono; // nanoseconds, system_clock, seconds
-
-#endif // SLEEPTIMER_HPP_INCLUDED
